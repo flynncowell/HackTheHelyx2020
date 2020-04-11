42c2651acd0df08bf361057039487b9cb8fb18a3
Young Scientists Journal <production@ysjournal.com>
2020-04-11 20:58:10 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="HackTheHelyx",
+    version="1.0",
+    install_requires=[
+        "requests",
+        "flask",
+        "python-firebase",
+
+    ],
+    # ...
+)
